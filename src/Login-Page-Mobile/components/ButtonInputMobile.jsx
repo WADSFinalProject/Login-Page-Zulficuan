7871bf66caa85ba7ab003ee647b973c7f661640b
Michael Bengawan <michaelbengawan1@gmail.com>
2024-06-18 22:26:46 +0700

    yes

--- src/Login-Page-Mobile/components/ButtonInputMobile.jsx
+++ src/Login-Page-Mobile/components/ButtonInputMobile.jsx
@@ -1,4 +1,5 @@
 import { Button} from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 // eslint-disable-next-line react/prop-types
 function ButtonInputMobile({label}) {
@@ -11,8 +12,15 @@ function ButtonInputMobile({label}) {
             backgroundColor: '#BDD8B0',
         },
     }
+
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate('/XYZManager')
+    }
+
     return (
-        <Button variant="contained" sx={style}>{label}</Button>
+        <Button variant="contained" sx={style} onClick={handleClick}>{label}</Button>
     )
 }
 export default ButtonInputMobile
